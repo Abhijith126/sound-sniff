b0b2435d69facaf38cdd9ff159484e9242c26ee9
Abhijith <akshay.abhijith@gmail.com>
2025-04-15 22:23:17 +0200

    Fix deploy issues and local issues

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -2,6 +2,8 @@ import i18n from 'i18next';
 import HttpBackend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
+const isDev = import.meta.env.DEV;
+
 i18n
   .use(HttpBackend)
   .use(initReactI18next)
@@ -12,6 +14,11 @@ i18n
     defaultNS: 'common',
     interpolation: {
       escapeValue: false,
+    },
+    backend: {
+      loadPath: isDev
+        ? '/locales/{{lng}}/{{ns}}.json'
+        : '/sound-sniff/locales/{{lng}}/{{ns}}.json',
     },
   });
 
--- vitest.config.mts
+++ vitest.config.mts
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'url';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/sound-sniff',
   plugins: [react()],
   test: {
     globals: true,
